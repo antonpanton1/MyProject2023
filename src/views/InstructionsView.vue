9b477f59c4ccf321018e595b6ea74aef450733e9
sebvstianmendez <119059170+sebvstianmendez@users.noreply.github.com>
2023-12-08 14:14:59 +0100

    instructions change

--- src/views/InstructionsView.vue
+++ src/views/InstructionsView.vue
@@ -4,7 +4,7 @@
         <h1 class="instructions-heading_one">Instructions</h1>
         
         <header>
-            <h1>Game ANTON Instructions</h1>
+            <h1>Game Instructions</h1>
         </header>
 
         <h2>Overview</h2>
