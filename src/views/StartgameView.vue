b116d6a135184efb707c07997ce47276f9b012cb
antonpanton1 <anton.ahlsson@icloud.com>
2023-12-12 15:28:07 +0100

    knap flytt submit

--- src/views/StartgameView.vue
+++ src/views/StartgameView.vue
@@ -9,28 +9,28 @@
         <label for="Question 1">
           Question 1:
         </label><br>
-        <input type="text" id="Question 1" v-model="question1" requeried="required" placeholder="Good questions only pls">
+        <input type="text" id="Question 1" v-model="qu1" requeried="required" placeholder="Good questions only pls">
         {{ question1 }}
       </p>
       <p v-if= "questionNR > 0 ">
         <label for="Question 2">
           Question 2:
         </label><br>
-        <input typ="text" id="Question 2" v-model="question2" requeried="required" placeholder="Good pls">
+        <input typ="text" id="Question 2" v-model="qu2" requeried="required" placeholder="Good pls">
         {{ question2 }}
       </p>
       <p v-if= "questionNR > 1 ">
         <label for="Question 3">
           Question 3:
         </label><br>
-        <input typ="text" id="Question 3" v-model="question3" requeried="required" placeholder="Good questions only pls">
+        <input typ="text" id="Question 3" v-model="qu3" requeried="required" placeholder="Good questions only pls">
         {{ question3 }}
       </p>
       <p v-if= "questionNR > 2 ">
         <label for="Question 4">
           Question 4:
         </label><br>
-        <input typ="text" id="Question 4" v-model="question4" requeried="required" placeholder="Good questions only pls">
+        <input typ="text" id="Question 4" v-model="qu4" requeried="required" placeholder="Good questions only pls">
         {{ question4 }}
       </p>
     
@@ -41,28 +41,28 @@
         <label for="Answer 1">
          Answer 1:
          </label><br>
-         <input type="number" id="Answer 1" v-model="answer1" required="required" placeholder="Your answer here">
+         <input type="number" id="Answer 1" v-model="an1" required="required" placeholder="Your answer here">
          {{ answer1 }}
       </p>  
       <p v-if= "questionNR > 0 ">
         <label for="Answer 2">
          Answer 2:
          </label><br>
-         <input type="number" id="Answer 2" v-model="answer2" required="required" placeholder="Your answer here">
+         <input type="number" id="Answer 2" v-model="an2" required="required" placeholder="Your answer here">
          {{ answer2 }}
       </p>  
       <p v-if= "questionNR > 1 ">
         <label for="Answer 3">
          Answer 3:
          </label><br>
-         <input type="number" id="Answer 3" v-model="answer3" required="required" placeholder="Your answer here">
+         <input type="number" id="Answer 3" v-model="an3" required="required" placeholder="Your answer here">
          {{ answer3 }}
       </p>  
       <p v-if= "questionNR > 2 ">
         <label for="Answer 4">
          Answer 4:
          </label><br>
-         <input type="number" id="Answer 4" v-model="answer4" required="required" placeholder="Your answer here">
+         <input type="number" id="Answer 4" v-model="an4" required="required" placeholder="Your answer here">
          {{ answer4 }}
       </p> 
     </div>
@@ -74,7 +74,9 @@
     <button type="submit" id= "sub" v-on:click="questionNR--" style="text-decoration: none; color: inherit; display: inline-block; padding: 10px; background-color: #af1111; color: #fff; border: none; cursor: pointer;width: 80px">
                   Remove Question
     </button>
-    <router-link class="WaitingRoomButton" :to="'/waitingroom/'" style="text-decoration: none; color: inherit; display: inline-block; padding: 10px; background-color: #ed2626; color: #fff; border: none; cursor: pointer;">Join Waiting Room</router-link>
+    <div class="submit">
+      <router-link class="WaitingRoomButton" :to="'/waitingroom/'" style="text-decoration: none; color: inherit; display: inline-block; padding: 10px; background-color: #ed2626; color: #fff; border: none; cursor: pointer;">Join Waiting Room</router-link>
+    </div> 
   </div>
   
 </template>
@@ -104,10 +106,10 @@ export default {
 <style>
 .question-view_background {
   background-image: linear-gradient(to bottom right, red, yellow);
-  display: flex;
+  /*display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: center;*/
   height: 100vh;
 }
 
@@ -136,5 +138,10 @@ export default {
   box-sizing: border-box;
   float: right;
 
+}
+
+.submit{
+  text-align: center;
+  margin-top: 30vh;
 }
 </style>
