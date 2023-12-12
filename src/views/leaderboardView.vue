087da5c72145a66faac21e69edf1ea69f72e3c30
antonpanton1 <anton.ahlsson@icloud.com>
2023-12-12 17:06:53 +0100

    leaderboard

---
+++ src/views/leaderboardView.vue
@@ -0,0 +1,56 @@
+<template>
+
+   <div class="start-view">
+      <h1 class="game-title">Leaderboard: </h1>
+  
+    <div class="users">
+        <h2>Namn: </h2>
+        <!--lista med allas username-->
+    </div>
+     
+    <div class="points">
+      <h2>Poäng: </h2>
+      <!--lista med allas poäng-->
+    </div>
+    <img src= "https://banner2.cleanpng.com/20180422/qxw/kisspng-gold-medal-olympic-medal-clip-art-honors-5adc14f33dc5a6.4712063515243727232531.jpg" alt="Span" title = "Another in-line element" style="width: 200px;">
+</div> 
+</template>
+
+
+<script>
+
+</script>
+
+<style >
+.start-view {
+  background-image: linear-gradient(to bottom right, red, yellow);
+  color: rgb(28, 28, 28);
+  /*display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;*/
+  height: 100vh;
+}
+
+.game-title {
+  color: black;
+  font-size: 50px;
+  margin-bottom: 00px;
+  position: center;
+  margin-top: -7px;
+}
+
+.users{
+  width: 50%;
+  box-sizing: border-box;
+  float: left;
+  margin-bottom: 350px;
+}
+.points{
+  width: 50%;
+  box-sizing: border-box;
+  float: right;
+  margin-bottom: 350px;
+}
+
+</style>
