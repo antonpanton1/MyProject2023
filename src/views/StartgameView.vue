b9530ecb013b3d4587a935c76e1611b5a1b1b32c
sebvstianmendez <zzebbe@gmail.com>
2023-12-12 14:26:08 +0100

    tho

--- src/views/StartgameView.vue
+++ src/views/StartgameView.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="question-view_background">
-    <h1 v-if="showMessage" class="questions-view">you are the</h1> <br>
-    <div v-if="showMessage" class="text-container">
-      <h1 class="blacktext">main</h1>
-      <h1 class="plaintext">character.</h1>
-    </div>
 
     <div class="questionTitle">
-      <h2>Type your questions and answers bellow! </h2>
+      <h2>Type your questions and answers below! </h2>
     </div>    
     <div class="questions">
       <p>
@@ -73,55 +68,19 @@
 import io from 'socket.io-client';
 const socket = io("localhost:3000");
 
-
-export default {
-  data() {
-    return {
-      showMessage: true,
-      questionNR: 0
-    };
-  },
-  mounted() {
-    // 3 sec timer
-    setTimeout(() => {
-      this.showMessage = false;
-    }, 3000);
-  }
-};
-
 </script>
 
 <style>
 .question-view_background {
   background-image: linear-gradient(to bottom right, red, yellow);
-  /*display: flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;*/
+  justify-content: center;
   height: 100vh;
 }
 
 .questions-view {
-  color: black;
-  font-size: 200px;
-  margin-bottom: -70px;
-  margin-top: -7px;
-}
-
-.text-container {
-  display: flex;
-  align-items: baseline;
-}
-
-.blacktext {
-  color: white;
-  font-size: 200px;
-  margin-bottom: -70px;
-  margin-top: -7px;
-  margin-right: 60px;
-}
-
-.plaintext {
   color: black;
   font-size: 200px;
   margin-bottom: -70px;
