7c9c5c13597fbd991559196aa7dda3e44f70857c
antonpanton1 <anton.ahlsson@icloud.com>
2023-12-16 16:56:19 +0100

    podium

--- src/views/ResultsView.vue
+++ src/views/ResultsView.vue
@@ -3,22 +3,44 @@
   <div class="result-view">
       <h1 class="title">Results: </h1>
     <div class="podium">
-      <svg width="200" height="250">
-        <rect width="200" height="150" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+      <svg width="210" height="250">
+        <rect width="200" height="150" style="fill:rgb(67, 67, 173);stroke-width:3;stroke:rgb(0,0,0)" />
+        <text x="50%" y="47%" alignment-baseline="bottom" text-anchor="middle" fill="white">2nd Place</text>
+        <text x="50%" y="10%" alignment-baseline="bottom" text-anchor="middle" fill="white">Namn på tvåan</text>
       </svg>
-      <svg width="200" height="330">
-        <rect width="200" height="230" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+
+      <svg width="210" height="330">
+        
+        <rect width="200" height="230" style="fill:rgb(56, 159, 109);stroke-width:3;stroke:rgb(0,0,0)" />
+        <text x="50%" y="60%" alignment-baseline="bottom" text-anchor="middle" fill="white">1st Place</text>
+        <text x="50%" y="10%" alignment-baseline="bottom" text-anchor="middle" fill="white">Namn på ettan</text>
       </svg>
-      <svg width="200" height="200">
-        <rect width="200" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
-      </svg>
+
+      <svg width="210" height="200">
+        <rect width="200" height="100" style="fill:rgb(229, 78, 169);stroke-width:3;stroke:rgb(0,0,0)" />
+        <text x="50%" y="35%" alignment-baseline="bottom" text-anchor="middle" fill="white">3rd Place</text>
+        <text x="50%" y="10%" alignment-baseline="bottom" text-anchor="middle" fill="white">Namn på trean</text>
+      </svg>        
     </div>  
     <div class="losers">
-        <h2>Losers: </h2>
+        <h2 style="margin-top: -80px;">Losers: </h2>
+        <ul>
+          <li>den som kom fyra</li>
+          <li>den som kom femma</li>
+          <li>den som kom sexa</li>
+          <li>osv</li>
+        </ul>
+        
         <!--lista med placering och användarnman för de utanför top 3-->
-    </div>
-         
+    </div> 
   </div> 
+   
+    
+         
+ 
+
+
+
 </template>
 
 
@@ -46,11 +68,11 @@
   padding:30px;
 }
 
-.users{
-  width: 50%;
+.losers{
+  width: 40%;
   box-sizing: border-box;
   float: left;
-  margin-bottom: 350px;
+  margin-bottom: 5vh;
 }
 
 </style>
