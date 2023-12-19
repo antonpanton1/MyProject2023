b329d302265ce2297869d271a3a3e7e12114c92a
IdaLarsson1 <ida.larsson.5232@student.uu.se>
2023-12-19 13:32:34 +0100

    fixade till knapparna på förstasidan med css

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -4,15 +4,21 @@
     <h1 class="game-title">0-100</h1>
     <h2 class="subheading">{{ uiLabels.subHeading }}</h2>
 
-    <button class="start-button" @click="startGame"><router-link v-bind:to="'/createlobby/'" style="text-decoration: none; color: inherit;">{{uiLabels.createGame}}</router-link></button><br>
-    <button class="join-button" @click="joinGame"><router-link v-bind:to="'/joingame/'" style="text-decoration: none; color: inherit;">{{uiLabels.joinGameWithCode}}</router-link></button>
-  
+   <!--  <button class="start-button" @click="startGame"><router-link v-bind:to="'/createlobby/'" style="text-decoration: none; color: inherit;">{{ uiLabels.createGame }}</router-link></button><br>
+    <button class="join-button" @click="joinGame"><router-link v-bind:to="'/joingame/'" style="text-decoration: none; color: inherit;">{{ uiLabels.joinGameWithCode }}</router-link></button>
+   -->
+    <!-- nya knappar-->
 
-    <button class="language-button" v-on:click="switchLanguage">{{ uiLabels.changeLanguage }}</button>
-   <!-- gamla instruktionsknapppen
+    <router-link class="big-button" :to="'/createlobby/'">{{ uiLabels.createGame }}</router-link><br>
+    <router-link class="big-button" :to="'/joingame/'">{{ uiLabels.joinGameWithCode }}</router-link>
+    
+
+    <button class="small-button" v-on:click="switchLanguage" style="left: 40px;">{{ uiLabels.changeLanguage }}</button>
+   
+    <!-- gamla instruktionsknapppen
     <router-link class="instructions-button" :to="'/instructions/'" style="text-decoration: none; color: inherit; display: inline-block; padding: 10px; background-color: #ed2626; color: #fff; border: none; cursor: pointer;">{{uiLabels.instructions}}</router-link>
   --> 
-    <router-link class="instructions-button" :to="'/instructions/'">{{ uiLabels.instructions }}</router-link>
+    <router-link class="small-button" :to="'/instructions/'" style="right: 40px;">{{ uiLabels.instructions }}</router-link>
     
 
   </div> 
@@ -103,7 +109,18 @@ export default {
   cursor: default;
 }
 
-button {
+/* button {
+  text-decoration: none;
+  padding: 20px 20px;
+  font-size: 1.5em;
+  background-color: darkorange;
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 8px;
+} */
+
+.big-button {
   text-decoration: none;
   padding: 20px 20px;
   font-size: 1.5em;
@@ -114,7 +131,8 @@ button {
   border-radius: 8px;
 }
 
-/* .start-button {
+/* .join-button {
+  text-decoration: none;
   padding: 20px 20px;
   font-size: 1.5em;
   background-color: darkorange;
@@ -123,14 +141,8 @@ button {
   cursor: pointer;
   border-radius: 8px;
 } */
-/* 
-.join-button {
-  padding: 20px 20px;
-  font-size: 1.5em;
-  background-color: darkorange;
-}
- */
-.instructions-button {
+
+/* .instructions-button {
   text-decoration: none;
   top: 30px;
   right: 40px;
@@ -142,9 +154,21 @@ button {
   border: none;
   cursor: pointer;
   border-radius: 8px;
+} */
+
+.small-button {
+  text-decoration: none;
+  top: 30px;
+  padding: 10px 20px;
+  font-size: 1em;
+  position: absolute;
+  background-color: #ed2626;
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 8px;
 }
-
-
+/* 
 .language-button {
   top: 30px;
   left: 40px;
@@ -152,20 +176,14 @@ button {
   font-size: 1em;
   position: absolute;
   background-color: #ed2626;
-}
+} */
 
-.start-button:hover {
+.big-button:hover {
   background-color: #F05E16;
 }
-.join-button:hover {
-  background-color: #F05E16;
-}
-.instructions-button:hover {
+.small-button:hover {
   background-color: #db0700;
 }
 
-.language-button:hover {
-  background-color: #db0700;
-}
 
 </style>
