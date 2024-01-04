14d8608f72464127da063032b8f8d01a195285d4
sebvstianmendez <zzebbe@gmail.com>
2024-01-04 11:29:03 +0100

    InstructionsUpdate

--- src/views/InstructionsView.vue
+++ src/views/InstructionsView.vue
@@ -1,21 +1,45 @@
 <template>
     <div class="background">
     <div class="rectangle">
+
+      <div class=" ins-tex">
         <h1 class="instructions-heading_one">{{ uiLabels.instructions }}</h1>
 
         <h2>{{ uiLabels.overview }}</h2>
-            <p>{{ uiLabels.overviewText }}</p>
+            <p>{{ uiLabels.oT1 }}</p>
+            <p>{{ uiLabels.oT2 }}</p>
+            <p>{{ uiLabels.oT3 }}</p>
+            <p>{{ uiLabels.oT4 }}</p>
+            <p>{{ uiLabels.oT5 }}</p>
+            <p>{{ uiLabels.oT6 }}</p>
+
         <h2>{{ uiLabels.how }}</h2>
     <ul>
-      <h>{{ uiLabels.howText }}</h> <br>
+      <p>{{ uiLabels.hT1 }}</p>
+      <p>{{ uiLabels.hT2 }}</p>
+      <p>{{ uiLabels.hT3 }}</p>
+      <p>{{ uiLabels.hT4 }}</p>
     </ul>
 
-        <h2>Tips</h2>
-      <p>{{ uiLabels.tipsText }}</p>
+        <h2>{{uiLabels.tips}}</h2>
+      <p>{{ uiLabels.tT1 }}</p>
+      <p>{{ uiLabels.tT2 }}</p>
+      <p>{{ uiLabels.tT3 }}</p>
+      <p>{{ uiLabels.tT4 }}</p>
+      <p>{{ uiLabels.tT5 }}</p>
+
     </div>
+
+      <div class="small-button-container">
+    <router-link class="small-button" :to="'//'">{{ uiLabels.return }}</router-link><br>
+    
+    </div>
+    
+</div>
 
 </div>
     
+
 
 </template>
 
@@ -56,7 +80,7 @@ export default {
 .rectangle {
 
       width: 90%;
-      height: 90vh;
+      height: 250%;
       background-color: #ffffff8b;
       border-radius: 1vw; /* rounded corners */
       margin: 0.1vh auto 0; /* margin top, content centered, 0 for sides */
@@ -85,10 +109,39 @@ body {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  height: 180vh;
+
 }
 
 .instructions-heading_one {
   font-size: 7vw;
 }
+
+.small-button-container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 10vh;
+}
+
+.small-button {
+  text-decoration: none;
+  padding: 2vw 2vw;
+  font-size: 1.5em;
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 1vw;
+  padding: 10px 20px;
+  font-size: 1em;
+  background-color: #ed2626;
+}
+
+.ins-text {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 10vh;
+}
+
 </style>
